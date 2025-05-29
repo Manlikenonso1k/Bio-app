71ba2c7d8410632603d7635ae5e7022f94b08183
Manlikenonso1k <victorynonso9@gmail.com>
2025-05-29 02:49:54 +0100

    Fixed routes and added niggas view

---
+++ Bio_network/resources/views/niggas/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Available Niggas | Home</title>
+</head>
+<body>
+    <h2>List of Niggas</h2>
+    
+    <ul>
+        <li>
+            Niggas here
+        </li>    
+    </ul>
+</body>
+</html>
--- Bio_network/routes/web.php
+++ Bio_network/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Builder\Function_;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/niggas',function(){
+    return view('niggas.index');
 });
