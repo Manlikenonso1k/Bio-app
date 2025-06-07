d231731a6871ec41419df96ff6429527e4baa0ae
Manlikenonso1k <victorynonso9@gmail.com>
2025-06-07 08:18:41 +0100

    to fix the header styling, i commented it out

--- Bio_network/resources/views/dashboard.blade.php
+++ Bio_network/resources/views/dashboard.blade.php
@@ -1,50 +1,5 @@
 <x-app-layout>
     {{-- Custom full header outside the slot for proper styling --}}
-    <header>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-      <nav class="middle-section">
-        <div class="left-section">
-          <a href="{{ route('dashboard') }}">
-            <img src="{{ asset('images/dashboard.svg') }}" alt="Dashboard Icon">
-          </a>
-        </div>
-        <div class="nav-content">
-          <div class="hamburger-menu">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-          </div>
-          <div class="nav-list">
-            <div class="nav-list-list">
-              <div>Resume</div>
-              <div>Cover Letter</div>
-              <div>Career Support</div>
-              <div>Blog</div>
-              <div>Financing</div>
-              <div class="last-div">Resources</div>
-            </div>
-            <div class="right-section">
-              <button class="Sign">Sign in</button>
-              <button class="get">Get Started</button>
-            </div>
-          </div>
-          <div class="mobile-nav-menu">
-            <div><a href="#">Resume</a></div>
-            <div><a href="#">Cover Letter</a></div>
-            <div><a href="#">Career Support</a></div>
-            <div><a href="#">Blog</a></div>
-            <div><a href="#">Financing</a></div>
-            <div><a href="#">Resources</a></div>
-            <div class="mbutton">
-              <button class="Sign">Sign in</button>
-              <button class="get">Get Started</button>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
-
     {{-- Optional page header slot for smaller header text --}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
